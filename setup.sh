4f66890ce634b77f784027e290525734c5f5d720
Thibaut Tiberghien <thibaut@planecq.com>
2014-04-10 07:07:19 +0000

    Change to own forked dotfile repo

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/tibotiber/ec2-dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
