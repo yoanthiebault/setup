e5cba7348f9ac1db6c87300c22ef7546ba83e836
Thibaut <thibaut@planecq.com>
2014-04-25 18:00:32 +0800

    Fix 'dotfiles' folder naming issue on git clone

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/tibotiber/ec2-dotfiles.git
+git clone https://github.com/tibotiber/ec2-dotfiles.git dotfiles
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
