a8fd025904e85f298cc83f1bb1eb84f4de0ea848
Thibaut <thibaut@planecq.com>
2014-04-25 21:03:27 +0800

    Add sudo rights for global install of jshint

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ sudo apt-get install -y nodejs
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
-npm install -g jshint
+sudo npm install -g jshint
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
