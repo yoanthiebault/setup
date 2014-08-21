9020929e612636a9d0b5fd706722c9eeb696b86f
Thibaut Tiberghien <thibaut@planecq.com>
2014-08-21 22:29:48 +0800

    Fix bug on zpresto init

--- configureZSH.sh
+++ configureZSH.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/.zprezto/runcoms/^README.md(.N); do
+ln -s ~/dotfiles/.zprezto/ ~/
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     rm -f "${ZDOTDIR:-$HOME}/.${rcfile:t}"
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
